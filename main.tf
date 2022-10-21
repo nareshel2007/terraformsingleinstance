c35c5396a11c236e30d64b59f04d4e2556243675
Naresh <43409239+nareshel2007@users.noreply.github.com>
2022-10-21 19:39:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,22 +119,22 @@ resource "aws_instance" "web" {
 # }
 
 
-   #resource "aws_instance" "Naresh-1" {
+     resource "aws_instance" "Naresh-1" {
        # ami = "ami-0b00d49148fb2a641"
       #ami = "ami-0d857ff0f5fc4e03b"
-      #ami = "${data.aws_ami.my_ami.id}"
+      ami = "${data.aws_ami.my_ami.id}"
        #ami = "${AMI ID}"
     #availability_zone = "us-east-1a"
-    #instance_type = "t2.micro"
-   #key_name = "JMS-35"
-   #subnet_id = "${aws_subnet.subnet1-public.id}"
-  # vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
- #  associate_public_ip_address = true	
-   # tags = {
-         #Name = "Naresh-1"
-         #Env = "dev"
-          #Owner = "Naresh"
- 	#CostCenter = "India"
+     instance_type = "t2.micro"
+    key_name = "JMS-35"
+    subnet_id = "${aws_subnet.subnet1-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+   associate_public_ip_address = true	
+     tags = {
+          Name = "Naresh-1"
+          Env = "dev"
+           Owner = "Naresh"
+ 	 
      }
 }
   }
