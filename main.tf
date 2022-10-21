c3af9f0a1d7c85e8d7e31f9775451457e211e2a0
Naresh <43409239+nareshel2007@users.noreply.github.com>
2022-10-21 18:16:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_security_group" "allow_all" {
 
 
    resource "aws_instance" "Naresh-1" {
-        ami = "var.imagename"
+        ami = "ami-0b00d49148fb2a641"
       #ami = "ami-0d857ff0f5fc4e03b"
       #ami = "${data.aws_ami.my_ami.id}"
        #ami = "${AMI ID}"
